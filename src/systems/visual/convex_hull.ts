c3a541dd24d9c6ff0d16b148839cd71268b4e74c
Akram Izzeldin <akram.izz.kh@gmail.com>
2020-06-19 22:19:55 +0200

    move convex hull algorithm to a seperate class

---
+++ src/systems/visual/convex_hull.ts
@@ -0,0 +1,129 @@
+import { NodeEntity } from './entity';
+
+export class GraphConvexHulls {
+  private _ran = false;
+  private nodes_keys: Array<string>;
+  private line_ends: Array<string>;
+
+  constructor(private nodes: Map<string, NodeEntity>) {
+    if (this.nodes.size == 0) {
+      this.nodes_keys = [];
+      this.line_ends = [];
+    } else {
+      this.nodes_keys = Array.from(this.nodes.keys());
+      this.nodes_keys.sort(this.comparePoints);
+      this.line_ends = Object.assign([], this.nodes_keys);
+    }
+  }
+
+  run() {
+    for (let i = 0; i < this.nodes_keys.length; ++i) {
+      if (this.nodes_keys[i] == this.line_ends[i]) {
+        this.convexHull(i);
+      }
+    }
+  }
+
+  get() {
+    if (!this._ran) {
+      this.run();
+      this._ran = true;
+    }
+
+    return [this.nodes_keys, this.line_ends];
+  }
+
+  convexHull(pos: number = 0) {
+    let start = this.nodes.get(this.nodes_keys[pos])!.position;
+    let current = start;
+    let current_id = pos;
+    let collinear = Array<number>();
+    let result = Array<number>();
+
+    while (true) {
+      let nextTarget = this.getNodePosition(pos);
+      let next_id = pos;
+
+      for (let i = pos; i < this.nodes_keys.length; i++) {
+        if (i == current_id) continue;
+        if (this.nodes_keys[i] != this.line_ends[i]) continue;
+        let val = this.crossProduct(
+          current,
+          nextTarget,
+          this.getNodePosition(i)
+        );
+
+        if (val > 0) {
+          nextTarget = this.getNodePosition(i);
+          next_id = i;
+          collinear = [];
+        } else if (val == 0) {
+          if (this.distance(current, nextTarget, this.getNodePosition(i)) < 0) {
+            collinear.push(next_id);
+            nextTarget = this.getNodePosition(i);
+            next_id = i;
+          } else {
+            collinear.push(i);
+          }
+        }
+      }
+
+      for (let i = 0; i < collinear.length; i++) {
+        if (result.length > 0 && collinear[i] == result[0]) continue;
+        result.push(collinear[i]); //add allpoints in collinear points to result set
+      }
+
+      if (next_id == pos) break;
+      result.push(next_id);
+      current = nextTarget;
+      current_id = next_id;
+    }
+
+    for (let i = 0; i < result.length; i++) {
+      this.line_ends[result[i]] = this.nodes_keys[
+        result[(i + 1) % result.length]
+      ];
+    }
+  }
+
+  getNodePosition = (index: number) => {
+    return this.nodes.get(this.nodes_keys[index])!.position;
+  };
+
+  comparePoints = (a: string, b: string) => {
+    let x1 = this.nodes.get(a)!.position.x;
+    let y1 = this.nodes.get(a)!.position.y;
+
+    let x2 = this.nodes.get(b)!.position.x;
+    let y2 = this.nodes.get(b)!.position.y;
+
+    if (x1 > x2 || (x1 == x2 && y1 > y2)) {
+      return 1;
+    } else if (x1 < x2 || (x1 == x2 && y1 < y2)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  crossProduct(a: any, b: any, c: any) {
+    let y1 = a.y - b.y;
+    let y2 = a.y - c.y;
+    let x1 = a.x - b.x;
+    let x2 = a.x - c.x;
+    return y2 * x1 - y1 * x2;
+  }
+
+  distance(a: any, b: any, c: any) {
+    let y1 = a.y - b.y;
+    let y2 = a.y - c.y;
+    let x1 = a.x - b.x;
+    let x2 = a.x - c.x;
+    let item1 = y1 * y1 + x1 * x1;
+    let item2 = y2 * y2 + x2 * x2;
+
+    if (item1 == item2) return 0;
+    else if (item1 < item2) return -1;
+    return 1;
+  }
+}
